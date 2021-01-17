03daf16e8ce9d5a55a73b57c460a9e3422a4f1a7
oceaster <oceaster@gmail.com>
2021-01-17 14:20:47 +0000

    fixed error forbidden travel message displayed when return date earlier than travel date.

--- static/js/pages/index.js
+++ static/js/pages/index.js
@@ -98,6 +98,8 @@ const validateDestination = async () => {
                     console.log(data);
                     if (data[loc] < data[des] && eligibility) {
                         return goodResult();
+                    } else if (data[loc] < data[des]) {
+                        return badResult();
                     } else {
                         addWarning(`Your current location is forbidden
                             from traveling to your selected destination`
