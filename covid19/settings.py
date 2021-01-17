46f34d4e0f17b54e198d17eb942a112e7403c2ca
oceaster <oceaster@gmail.com>
2021-01-17 14:49:38 +0000

    added HTTPS to cors-headers for production

--- covid19/settings.py
+++ covid19/settings.py
@@ -100,5 +100,5 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8000',
     'http://127.0.0.1:8000',
-    'eastercompany.eu.pythonanywhere.com'
+    'https://eastercompany.eu.pythonanywhere.com'
 )
